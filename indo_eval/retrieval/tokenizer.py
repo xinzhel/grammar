ea4203e014427478e952d009d06cf81a0226220f
xinzhel <43598514+xinzhel@users.noreply.github.com>
2024-03-09 12:21:28 +1100

    Update tokenizer.py

--- indo_eval/retrieval/tokenizer.py
+++ indo_eval/retrieval/tokenizer.py
@@ -9,7 +9,7 @@ import spacy
 from spacy.cli.download import download as spacy_download
 logger = logging.getLogger(__name__)
 
-LOADED_SPACY_MODELS: Dict[Tuple[str, bool, bool, bool], SpacyModelType] = {}
+
 def get_spacy_model(
     spacy_model_name: str="en_core_web_sm", pos_tags: bool = True, parse: bool = False, ner: bool = False
 ) -> SpacyModelType:
@@ -18,31 +18,23 @@ def get_spacy_model(
     keyed by the options we used to create the spacy model, so any particular configuration only
     gets loaded once.
     """
+    disable = ["vectors", "textcat"]
+    if not pos_tags:
+        disable.append("tagger")
+    if not parse:
+        disable.append("parser")
+    if not ner:
+        disable.append("ner")
+    try:
+        spacy_model = spacy.load(spacy_model_name, disable=disable)
+    except OSError:
+        spacy_download(spacy_model_name)
+        # Import the downloaded model module directly and load from there
+        spacy_model_module = __import__(spacy_model_name)
+        spacy_model = spacy_model_module.load(disable=disable)  # type: ignore
 
-    options = (spacy_model_name, pos_tags, parse, ner)
-    if options not in LOADED_SPACY_MODELS:
-        disable = ["vectors", "textcat"]
-        if not pos_tags:
-            disable.append("tagger")
-        if not parse:
-            disable.append("parser")
-        if not ner:
-            disable.append("ner")
-        try:
-            spacy_model = spacy.load(spacy_model_name, disable=disable)
-        except OSError:
-            logger.warning(
-                f"Spacy models '{spacy_model_name}' not found.  Downloading and installing."
-            )
-            spacy_download(spacy_model_name)
-
-            # Import the downloaded model module directly and load from there
-            spacy_model_module = __import__(spacy_model_name)
-            spacy_model = spacy_model_module.load(disable=disable)  # type: ignore
-
-        LOADED_SPACY_MODELS[options] = spacy_model
-    return LOADED_SPACY_MODELS[options]
-
+    return spacy_model
+    
 def _remove_spaces(tokens: List[spacy.tokens.Token]) -> List[spacy.tokens.Token]:
     return [token for token in tokens if not token.is_space]
 
